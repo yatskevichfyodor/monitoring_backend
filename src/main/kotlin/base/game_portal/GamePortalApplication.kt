3ce32e3aff49ca0dd9cfd7cb242a5c07bc2f779d
fedor <yatskevichfyodor@gmail.com>
2019-02-05 23:58:41 +0300

    Initial commit

---
+++ src/main/kotlin/base/game_portal/GamePortalApplication.kt
@@ -0,0 +1,12 @@
+package base.game_portal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamePortalApplication
+
+fun main(args: Array<String>) {
+    runApplication<GamePortalApplication>(*args)
+}
+
---
+++ src/test/kotlin/base/game_portal/GamePortalApplicationTests.kt
@@ -0,0 +1,17 @@
+package base.game_portal
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GamePortalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
